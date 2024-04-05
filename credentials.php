ea93fda3b538edc94b7043972a318c2e8ef72102
Din Nguyen <dinanddanaretwins@gmail.com>
2024-04-05 00:21:27 +0000

    Initial commit

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
-<?php
-$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
-$dbname = 'employees';              //which db you're going to use
-$username = 'dbuser';
-$password = 'abc123';
+<?php                            //connection & credentials
+$servername = "localhost";      //mysql is on the same host as apache
+$dbname = "employees";          //which db you're going to use
+$username = "phpuser1";
+$password = "abc123";
 ?>
